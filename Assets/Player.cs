75d23651b123f1c51731d9b48aa9422c6e1c872a
watanabe18 <watanabe18@up.edu>
2016-03-20 21:58:20 -0700

    player walk animation
    added the animation. animation not working properly.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,9 +5,13 @@ public class Player : MonoBehaviour {
 
 	private Rigidbody rb;
 	public float speed;
+	public GameObject avatar;
+	private Animator animator;
 
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
+		animator = avatar.GetComponent<Animator> ();
+
 	}
 
 	void FixedUpdate(){
@@ -16,7 +20,15 @@ public class Player : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal * Time.deltaTime, 0.0f, moveVertical * Time.deltaTime);
 
+		if (movement != Vector3.zero) {
+			animator.SetBool ("isMoving", true);
+		} else {
+			animator.SetBool("isMoving", false);
+		}
+
 		transform.Translate (movement);
+
+
 
 	}
 
